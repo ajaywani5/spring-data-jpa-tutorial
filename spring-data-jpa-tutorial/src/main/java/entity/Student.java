868103c56c55cc286d9903334b1ec95300c74ef7
Ajay Wani <ajay.wani1986@gmail.com>
2022-11-29 21:58:49 +0530

    comment added

--- spring-data-jpa-tutorial/src/main/java/entity/Student.java
+++ spring-data-jpa-tutorial/src/main/java/entity/Student.java
@@ -10,7 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
+@Entity   //Entity created
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
