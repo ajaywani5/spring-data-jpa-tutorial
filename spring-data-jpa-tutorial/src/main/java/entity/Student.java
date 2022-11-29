bb76fb4d2e3f463b9a3d787eb45f8c8bc9452de4
Ajay Wani <ajay.wani1986@gmail.com>
2022-11-29 21:47:10 +0530

    Initial commit

---
+++ spring-data-jpa-tutorial/src/main/java/entity/Student.java
@@ -0,0 +1,35 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="tbl-student")
+public class Student {
+    @Id
+private Long studentId;
+    @Column(name="fName")
+private String firstName;
+    @Column(name="lName")
+private String lastName;
+    @Column(name="email")
+private String emailId;
+    @Column(name="gName")
+private String guardianName;
+    @Column(name="gEmail")
+private String guardianEmail;
+    @Column(name="gMobile")
+private String guardianMobile;
+
+}
